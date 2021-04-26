a50449c93b1f5eb8c190086b5f6b80e459f9cf90
saedjabali <saad.jabali@yahoo.com>
2021-04-26 13:59:47 +0300

    lab01

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Header extends React.Component {
     render() {
         return (
-            <h1>This is the Header</h1>
+            <h1>Horned Beasts</h1>
         );
     }
 }
--- src/Components/HornedBeasts.js
+++ src/Components/HornedBeasts.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 class HornedBeasts extends React.Component {
     render() {
-        return(
-        <div>
- <h2>{this.props.name}</h2>
- <img src={this.props.url} alt='' title={this.name} />
- <p>{this.props.description}</p>
- </div>
+        return (
+            <div>
+                <h2>{this.props.name}</h2>
+                <img src={this.props.url} alt='' title={this.name} />
+                <p>{this.props.description}</p>
+            </div>
         )
     }
-} 
+}
 export default HornedBeasts;
